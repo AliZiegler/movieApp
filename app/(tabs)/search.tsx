2cc7d8b609ac784104182e0be06ce472c4bdfab1
AliZiegler <ali12xx66@gmail.com>
2025-11-20 14:54:16 +0300

    add alot of stuff so i thought i should make a save to make sure

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function Search() {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text className="font-bold text-5xl text-dark-200">Search</Text>
+        </View>
+    )
+}
